f2d6ffd6ebdd2b428bfb13a87040d9f7362f36ef
Anony Kagamine <16967744+AnonyKagamine@users.noreply.github.com>
2020-04-19 16:15:37 +0000

    Create generate_index.py

---
+++ generate_index.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+import json
+from urllib.request import urljoin
+
+GH_PAGES_PREFIX = "https://anonykagamine.github.io"
+OUTPUT_FILENAME = "index.json"
+def main():
+    index_list = []
+    for filename in sys.stdin.readlines():
+        with open(filename, "r") as f:
+            jsonobj = json.load(f)
+            index_column = {}
+            index_column["title"] = jsonobj["_title"]
+            index_column["description"] = jsonobj["_description"]
+            index_column["provider"] = jsonobj["_provider"]
+            index_column["url"] = urljoin(GH_PAGES_INDEX, filename)
+
+            index_list.append(index_column)
+
+    with open(OUTPUT_FILENAME, "w") as f:
+        json.dump(index_list, f, indent=2, ensure_ascii=False)
+
+if __name__ == '__main__':
+    main()
